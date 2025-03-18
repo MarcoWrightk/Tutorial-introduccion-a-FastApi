6f1b7b73afa100e8a257ded121614c2a5eccf262
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-03-17 21:00:10 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+class Settings:
+    DATABASE_URL = "sqlite:///./test.db"
+    SECRET_KEY = "secretkeyexample"
+
+settings = Settings()
+
+#Explicación:
+#Se crea una clase Settings para almacenar configuraciones globales de la aplicación, lo que permite acceder a ellas fácilmente desde cualquier parte del código.
