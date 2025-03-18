e1c94428191e4b56235bb6d680f013c934639c3d
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-03-17 21:36:02 -0600

    Create main.py

---
+++ Ejemplo 5 basic error handling/main.py
@@ -0,0 +1,84 @@
+from fastapi import FastAPI, APIRouter, Query, HTTPException
+
+from typing import Optional, Any
+
+from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
+from app.recipe_data import RECIPES
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    """
+    return {"msg": "Hello, World!"}
+
+
+# Actualizado con manejo de errores
+# https://fastapi.tiangolo.com/tutorial/handling-errors/
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
+def fetch_recipe(*, recipe_id: int) -> Any:
+    """
+    Obtener una receta individual por ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if not result:
+        # Se lanza la excepción, no se retorna - de lo contrario se obtendría
+        # un error de validación.
+        raise HTTPException(
+            status_code=404, detail=f"Receta con ID {recipe_id} no encontrada"
+        )
+
+    return result[0]
+
+
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+def search_recipes(
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, examples="chicken"),
+    max_results: Optional[int] = 10,
+) -> dict:
+    """
+    Buscar recetas basadas en la palabra clave de la etiqueta
+    """
+    if not keyword:
+        # Usamos el corte de listas de Python para limitar los resultados
+        # basándonos en el parámetro de consulta max_results
+        return {"results": RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
+
+
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+    """
+    Crear una nueva receta (solo en memoria)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.dict())
+
+    return recipe_entry
+
+
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    # Usar esto solo para fines de depuración
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+
